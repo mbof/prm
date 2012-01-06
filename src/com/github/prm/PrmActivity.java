6906ee8759b8305b7b522286bbc2f810f7506080
Benoit <bdbkun@gmail.com>
2012-01-06 09:24:34 +0000

    Initial commit

---
+++ src/com/github/prm/PrmActivity.java
@@ -0,0 +1,13 @@
+package com.github.prm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PrmActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
