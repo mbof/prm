6fe6e4ac16e89b716f247c82b343b64a0572c623
Benoit <bdbkun@gmail.com>
2012-01-07 06:31:40 +0000

    Link button to push activity

--- src/com/github/prm/PrmActivity.java
+++ src/com/github/prm/PrmActivity.java
@@ -1,13 +1,40 @@
 package com.github.prm;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class PrmActivity extends Activity {
+	
+	/** Image chooser */
+	private Intent pictureChooser;
+	
+	/** Handler for click on image picker */
+	private OnClickListener chooseButtonClickListener;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Intent pictureChoosingIntent = new Intent(Intent.ACTION_GET_CONTENT)
+			.addCategory(Intent.CATEGORY_OPENABLE)
+			.setType("image/*");
+      
+        pictureChooser = Intent.createChooser(pictureChoosingIntent,
+        		getString(R.string.pick_image_action_title));
+        
+        chooseButtonClickListener = new OnClickListener() {
+        	public void onClick(View view) {
+        		startActivity(pictureChooser);
+        	}
+        };
+        
+        findViewById(R.id.pick_image_button)
+        	.setOnClickListener(chooseButtonClickListener);
+        
     }
 }
