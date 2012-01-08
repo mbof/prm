4a481671a9dc71fbb804a4f2abe2a94c4745153e
Benoit <bdbkun@gmail.com>
2012-01-08 17:11:46 +0000

    Activates sensor, untracks generated classes

--- src/com/github/prm/PrmActivity.java
+++ src/com/github/prm/PrmActivity.java
@@ -1,18 +1,27 @@
 package com.github.prm;
 
 import java.io.FileNotFoundException;
+import java.util.List;
+
+import com.github.prm.PrmActivity.Projector;
 
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PrmActivity extends Activity {
 
@@ -33,6 +42,8 @@ public class PrmActivity extends Activity {
 	
 	/** Handler for click on "Choose image" button  */
 	private OnClickListener chooseButtonClickListener;
+
+	private Projector projector;
 	
     /** Called when the activity is first created */
     @Override
@@ -60,6 +71,10 @@ public class PrmActivity extends Activity {
         preview = (ImageView) findViewById(R.id.preview);
         
         projectionView = (ImageView) findViewById(R.id.projection_view);
+        
+        projector = new Projector((Button) findViewById(R.id.start_button),
+        		(ImageView) findViewById(R.id.projection_view),
+        		(TextView) findViewById(R.id.projector_coordinates));
         
         Log.d("Prm", "All done");
     }
@@ -90,4 +105,69 @@ public class PrmActivity extends Activity {
     	}
     }
     
+    class Projector implements SensorEventListener, OnClickListener {
+    	
+    	private Button startButton;
+		private ImageView projectionView;
+		private TextView projectionTextView;
+		private boolean isProjecting;
+		private SensorManager sensorManager;
+		private Sensor rotationSensor;
+
+		public Projector(Button startButton, ImageView projectionView, TextView projectionTextView) {
+    		this.startButton = startButton;
+    		this.projectionView = projectionView;
+    		this.projectionTextView = projectionTextView;
+    		this.isProjecting = false;
+    		this.init();
+    	}
+		
+		protected void init() {
+    		startButton.setOnClickListener(this);
+    		projectionView.setBackgroundColor(android.R.color.darker_gray);
+    		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    		rotationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+    		if (rotationSensor == null)
+    			projectionTextView.setText("Error: no rotation sensor");
+		}
+    	
+    	synchronized public void start() {
+    		startButton.setText(getString(R.string.stop_button_text));
+    		projectionTextView.setText("Projecting...");
+    		projectionView.setBackgroundColor(android.R.color.white);
+    		sensorManager.registerListener(this, rotationSensor, SensorManager.SENSOR_DELAY_UI);
+    		this.isProjecting = true;
+    	}
+    	
+    	synchronized public void stop() {
+    		startButton.setText(getString(R.string.start_button_text));
+    		projectionTextView.setText("Stopped");
+    		this.isProjecting = false;
+    	}
+
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+			// Do nothing
+		}
+
+		public void onSensorChanged(SensorEvent event) {
+			String debugText = "Sensor: ";
+			for (int i = 0; i < event.values.length; i++)
+				debugText += event.values[i] + " ";
+			projectionTextView.setText(debugText);
+		}
+
+		public void onClick(View v) {
+			if(this.isProjecting)
+				this.stop();
+			else
+				this.start();
+		}
+    	
+    }
+    
+    protected void onPause() {
+    	super.onPause();
+    	projector.stop();
+    }
+        
 }
